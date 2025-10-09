1fdb78814ea51960d8a430dd9984819adb49192e
sagar <sagardabas2019@gmail.com>
2025-10-09 18:54:39 +0530

    completed the app

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 #!/usr/bin/env node
-import { program } from "commander" ;
-import fs from "fs";
-import path from "path";
-import chalk from "chalk";
+import readline from "readline";
 
-program
-    .option("-f,--file <path>", "enter your file path");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-program.parse(process.argv);
-const options = program.opts();
-
-if (!options.file) {
-    console.log(chalk.red(`There is no file in that directory`));
-    process.exit(1);
-} 
-
-const filePath = path.resolve(options.file); 
-
-if (!fs.existsSync(filePath)) {
-    console.log(chalk.red(`File not found! ${filePath}`));
-    process.exit(1);
+const todo = [];
+const showMenu = () => {
+    console.log("\n 1: Add a task");
+    console.log("\n 2: View all tasks");
+    console.log("\n 3: Exit");
+    rl.question("choose an option: ", handleInput);
 }
 
-try {
-    const data = fs.readFileSync(filePath, "utf-8");
-    console.log(chalk.green(`The length of characters: `), chalk.bgGreen(`${data.length}`));
-} catch (err) {
-    console.log(chalk.red(`there was an error`), chalk.bgRed(`${err}`));
+const handleInput = (option) => {
+    if (option === "1") {
+        rl.question("Enter the task: ", (task) => {
+            todo.push(task);
+            console.log(`Task added:`, task);
+            showMenu()
+        })
+    } else if (option === "2") {
+        console.log("\nYour todo list:");
+        todo.forEach((task, index) =>{
+            console.log(`${index + 1}.${task}`);
+        })
+        showMenu();
+    } else if (option === "3") {
+        console.log("GoodBye!");
+        rl.close();
+    } else {
+        console.log("Invalid option try again");
+        showMenu();
+    }
 }
+
+showMenu()
